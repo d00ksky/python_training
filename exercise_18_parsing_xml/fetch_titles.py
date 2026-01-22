0f926560e13525436fec5ebace114cb1b3a42ae0
d00ksky <debski.jakub@gmail.com>
2026-01-22 22:24:09 +0100

    exercise 18 fetch titles completed

---
+++ exercise_18_parsing_xml/fetch_titles.py
@@ -0,0 +1,24 @@
+import urllib.request
+import xml.etree.cElementTree as ET
+
+
+URL = "http://export.arxiv.org/api/query?search_query=all:transformer&start=0&max_results=5"
+
+with urllib.request.urlopen(URL) as response:
+    xml_bytes = response.read()
+    
+xml_text = xml_bytes.decode("utf-8")
+
+print(xml_text)
+
+root = ET.fromstring(xml_text)
+
+ns = {"a": "http://www.w3.org/2005/Atom"}
+
+
+entries = root.findall("a:entry", ns)
+
+for i, entry in enumerate(entries[:5], start=1):
+    title_el = entry.find("a:title", ns)
+    title = title_el.text.strip()
+    print(f"{i}, {title}")
